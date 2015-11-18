f20fa3e97e003ce83fd18c2bbf439f37d6274309
Max Joehnk <maxjoehnk@gmail.com>
2015-11-18 10:42:06 +0100

    Added dist files

---
+++ dist/angular-vibrant.js
@@ -0,0 +1,57 @@
+angular
+	.module('ngVibrant', []);
+
+angular
+    .module('ngVibrant')
+    .filter('rgbToHex', rgbToHex);
+
+function rgbToHex(rgb) {
+    if (angular.isArray(rgb)) {
+        var hex = '#';
+        hex += rgb[0].toString(16);
+        hex += rgb[1].toString(16);
+        hex += rgb[2].toString(16);
+        return hex;
+    }else {
+        return rgb;
+    }
+}
+rgbToHex.$inject = ['rgb'];
+
+angular
+    .module('ngVibrant')
+    .directive('vibrant', vibrant);
+
+function vibrant($vibrant) {
+    var directive = {
+        restrict: 'E',
+        scope: {
+            vibrant: '=' //Model
+        },
+        link: link
+    };
+
+    return directive;
+
+    function link(scope, element) {
+        scope.vibrant = [];
+        element.on('load', function() {
+            var swatches = $vibrant(element[0]);
+            scope.vibrant = swatches;
+        });
+    }
+}
+vibrant.$inject = ['$vibrant'];
+
+angular
+    .module('ngVibrant')
+    .provider('$vibrant', $vibrantProvider);
+
+function $vibrantProvider() {
+    this.$get = function() {
+        function $vibrant(element) {
+            var instance = new Vibrant(element);
+            return instance.swatches();
+        }
+    };
+}
