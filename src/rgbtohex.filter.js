83440850b5c06a53a65885c15061c670da833e5f
Max Joehnk <maxjoehnk@gmail.com>
2015-11-18 11:03:32 +0100

    rgbToHex is a factory function

--- src/rgbtohex.filter.js
+++ src/rgbtohex.filter.js
@@ -2,14 +2,16 @@ angular
     .module('ngVibrant')
     .filter('rgbToHex', rgbToHex);
 
-function rgbToHex(rgb) {
-    if (angular.isArray(rgb)) {
-        var hex = '#';
-        hex += rgb[0].toString(16);
-        hex += rgb[1].toString(16);
-        hex += rgb[2].toString(16);
-        return hex;
-    }else {
-        return rgb;
+function rgbToHex() {
+    return function(rgb) {
+        if (angular.isArray(rgb)) {
+            var hex = '#';
+            hex += rgb[0].toString(16);
+            hex += rgb[1].toString(16);
+            hex += rgb[2].toString(16);
+            return hex;
+        }else {
+            return rgb;
+        }
     }
 }
