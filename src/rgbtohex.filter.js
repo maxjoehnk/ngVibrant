9a6febc20bbe02c7d157eabde088f06bf37317f5
Max Joehnk <maxjoehnk@gmail.com>
2015-11-18 10:41:32 +0100

    Spaces instead of tabs

--- src/rgbtohex.filter.js
+++ src/rgbtohex.filter.js
@@ -1,15 +1,15 @@
 angular
-	.module('ngVibrant')
-	.filter('rgbToHex', rgbToHex);
+    .module('ngVibrant')
+    .filter('rgbToHex', rgbToHex);
 
 function rgbToHex(rgb) {
-	if (angular.isArray(rgb)) {
-		var hex = '#';
-		hex += rgb[0].toString(16);
-		hex += rgb[1].toString(16);
-		hex += rgb[2].toString(16);
-		return hex;
-	}else {
-		return rgb;
-	}
+    if (angular.isArray(rgb)) {
+        var hex = '#';
+        hex += rgb[0].toString(16);
+        hex += rgb[1].toString(16);
+        hex += rgb[2].toString(16);
+        return hex;
+    }else {
+        return rgb;
+    }
 }
