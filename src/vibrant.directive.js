eb5ca8c6cb2e4ed105564735733c5120ed033deb
Max Joehnk <maxjoehnk@gmail.com>
2015-11-19 14:54:06 +0100

    Allow usage as Attribute and Element, map swatches to ng-model and added url and color attributes

--- src/vibrant.directive.js
+++ src/vibrant.directive.js
@@ -4,20 +4,28 @@ angular
 
 function vibrant($vibrant) {
     var directive = {
-        restrict: 'A',
+        restrict: 'AE',
         scope: {
-            vibrant: '=' //Model
+            model: '=ngModel', //Model
+            url: '@?',
+            color: '@?'
         },
         link: link
     };
 
     return directive;
 
-    function link(scope, element) {
-        scope.vibrant = [];
-        element.on('load', function() {
-            var swatches = $vibrant(element[0]);
-            scope.vibrant = swatches;
-        });
+    function link(scope, element, attrs) {
+        scope.model = [];
+        if (angular.isDefined(attrs.url)) {
+            $vibrant.get(attrs.url).then(function(swatches) {
+                scope.model = angular.isDefined(attrs.color) ? swatches[attrs.color] : swatches;
+            });
+        }else {
+            element.on('load', function() {
+                var swatches = $vibrant.vibrant(element[0]);
+                scope.model = angular.isDefined(attrs.color) ? swatches[attrs.color] : swatches;
+            });
+        }
     }
 }
