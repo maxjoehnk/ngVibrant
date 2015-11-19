aa69b0386d02b31bc7fbbe47c1080170a42765f2
Max Joehnk <maxjoehnk@gmail.com>
2015-11-19 18:21:55 +0100

    Added quality and colors attributes

--- src/vibrant.directive.js
+++ src/vibrant.directive.js
@@ -8,7 +8,9 @@ function vibrant($vibrant) {
         scope: {
             model: '=ngModel', //Model
             url: '@?',
-            swatch: '@?'
+            swatch: '@?',
+            quality: '@?',
+            colors: '@?'
         },
         link: link
     };
@@ -17,13 +19,19 @@ function vibrant($vibrant) {
 
     function link(scope, element, attrs) {
         scope.model = [];
+        if (angular.isUndefined(attrs.quality)) {
+            attrs.quality = $vibrant.getDefaultQuality();
+        }
+        if (angular.isUndefined(attrs.colors)) {
+            attrs.colors = $vibrant.getDefaultColors();
+        }
         if (angular.isDefined(attrs.url)) {
             $vibrant.get(attrs.url).then(function(swatches) {
                 scope.model = angular.isDefined(attrs.swatch) ? swatches[attrs.swatch] : swatches;
             });
         }else {
             element.on('load', function() {
-                var swatches = $vibrant(element[0]);
+                var swatches = $vibrant(element[0], attrs.colors, attrs.quality);
                 scope.model = angular.isDefined(attrs.swatch) ? swatches[attrs.swatch] : swatches;
             });
         }
