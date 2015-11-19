4e6b4fcb6d9b32ba6cc466e69dae012c7bd52acd
Max Joehnk <maxjoehnk@gmail.com>
2015-11-19 14:54:43 +0100

    Fixed provider and added get method which takes a url and returns a promise

--- src/vibrant.service.js
+++ src/vibrant.service.js
@@ -3,17 +3,33 @@ angular
     .provider('$vibrant', $vibrantProvider);
 
 function $vibrantProvider() {
-    this.$get = function() {
-        function $vibrant(element) {
-            var instance = new Vibrant(element);
-            var swatches = instance.swatches();
-            var rgb = {};
-            Object.getOwnPropertyNames(swatches).forEach(function(swatch) {
-                if (angular.isDefined(swatches[swatch])) {
-                    rgb[swatch] = swatches[swatch].rgb;
-                }
-            });
-            return rgb;
-        }
+    this.$get = function($q, $document) {
+        return ({
+            get: function(url) {
+                var that = this;
+                return $q(function(resolve, reject) {
+                    var pic = $document[0].createElement('img');
+                    pic.src = url;
+                    $document.find('body').append(pic);
+                    var element = angular.element(pic);
+                    element.css({display: 'none'});
+                    element.on('load', function() {
+                        resolve(that.vibrant(pic));
+                    });
+                    element.on('error', reject);
+                });
+            },
+            vibrant: function(element) {
+                var instance = new Vibrant(element);
+                var swatches = instance.swatches();
+                var rgb = {};
+                Object.getOwnPropertyNames(swatches).forEach(function(swatch) {
+                    if (angular.isDefined(swatches[swatch])) {
+                        rgb[swatch] = swatches[swatch].rgb;
+                    }
+                });
+                return rgb;
+            }
+        });
     };
 }
